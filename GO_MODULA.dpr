9003209f06ba154afae7ead3199fd18e27d3c74a
angerp <paolo.angeretti@gestionaleopen.org>
2021-03-16 09:36:11 +0100

    GO MODULA 10.11.05

--- GO_MODULA.dpr
+++ GO_MODULA.dpr
@@ -1,28 +1,24 @@
 program GO_MODULA;
 
+{$R 'ZZVERSIONE_GO_EXE.res' '..\..\..\go_1011\src\ZZVERSIONE_GO_EXE.rc'}
 
-
-
-
-
-
-
-
-{$R 'ZZVERSIONE_GO_EXE.res' '..\..\..\go_1010\src_101002\ZZVERSIONE_GO_EXE.rc'}
 
 uses
   madExcept,
   Vcl.Forms,
-  DMARC in '..\..\..\go_1010\src_101002\DMARC.pas' {ARC: TDataModule},
-  GGLOGIN in '..\..\..\go_1010\src_101002\GGLOGIN.pas' {LOGIN},
-  GGBASE in '..\..\..\go_1010\src_101002\GGBASE.pas' {BASE},
-  GGFORMBASE in '..\..\..\go_1010\src_101002\GGFORMBASE.pas' {FORMBASE},
-  GGSTAMPA in '..\..\..\go_1010\src_101002\GGSTAMPA.pas' {STAMPA},
-  DMCLOUD in 'DMCLOUD.pas' {CLOUD: TDataModule},
-  FRMDITTA in 'FRMDITTA.pas' {DITTA: TFrame},
+  ZZACCESSO in '..\..\..\go_1011\src\ZZACCESSO.pas',
+  DMARC in '..\..\..\go_1011\src\DMARC.pas' {ARC: TDataModule} ,
+  GGLOGIN in '..\..\..\go_1011\src\GGLOGIN.pas' {LOGIN} ,
+  GGBASE in '..\..\..\go_1011\src\GGBASE.pas' {BASE} ,
+  GGFORMBASE in '..\..\..\go_1011\src\GGFORMBASE.pas' {FORMBASE} ,
+  GGELABORA in '..\..\..\go_1011\src\GGELABORA.pas' {ELABORA} ,
+  GGSTAMPA in '..\..\..\go_1011\src\GGSTAMPA.pas' {STAMPA} ,
+  DMCLOUD in 'DMCLOUD.pas' {CLOUD: TDataModule} ,
+  FRMDITTA in 'FRMDITTA.pas' {DITTA: TFrame} ,
   GGMENU in 'GGMENU.pas' {MENUGG};
 
-{$R *.res}
+{$r *.res}
+
 
 begin
   Application.Initialize;
@@ -31,4 +27,5 @@ begin
   Application.CreateForm(TARC, ARC);
   Application.CreateForm(TLOGIN, LOGIN);
   Application.Run;
-end.                                  \
+
+end.\
